06882cd47106c66df5f8d408769bbf699f9599be
santi <santi@DESKTOP-SCRENCQ>
2021-02-17 11:37:45 -0300

    Movement & Scenes

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager _Instance { get; private set; }
+
+    private void Awake()
+    {
+        _Instance = this;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void LevelOne()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("LevelOne");
+    }
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+
+}
---
+++ Assets/Scripts/Ship/ShipController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    ShipModel model;
+
+
+    void Start()
+    {
+        model = GetComponent<ShipModel>();
+    }
+
+   
+    void Update()
+    {
+        Movement();
+    }
+
+
+    public void Movement()
+    {
+
+        model.aceleration = Input.GetAxisRaw("Vertical");
+        model.dirHTwo = Input.GetAxisRaw("Horizontal");
+        model.dirV = 0;
+        model.dirH = 0;
+
+        model.currentV = Mathf.Lerp(model.currentV, model.dirV, model.interpolation * Time.deltaTime);
+        model.currentH = Mathf.Lerp(model.currentH, model.dirH, model.interpolation * Time.deltaTime);
+        model.turnLeftRigth = Mathf.Lerp(model.turnLeftRigth, model.dirHTwo, model.interpolation * Time.deltaTime);
+
+        transform.Rotate(model.currentV * model.turnSpeed * Time.deltaTime, model.turnLeftRigth * model.turnSpeed * Time.deltaTime, model.currentH * model.turnSpeed * Time.deltaTime);
+
+        if (model.aceleration > 0)
+        {
+            transform.position += transform.forward * model.speed * Time.deltaTime;
+        }
+    }
+
+
+    public void Rotate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * model.turnSpeed), Space.Self);
+        }
+    }
+
+}
